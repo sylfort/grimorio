4065cb528f89888f0daaab1e192b8dfd96af6c4e
Sylvio <sylviomafort@gmail.com>
2021-03-26 09:59:51 +0900

    created vue

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <Header></Header>
+  <router-view />
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { Header } from '@/components/Header.vue';
+
+export default defineComponent({
+  components: { Header },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  components: { },
+
+  setup() {
+    return {
+
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.header {
+
+}
+</style>
